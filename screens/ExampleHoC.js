50d9976233d9217641f5f97fd2b0dc2d7f10f5a5
Pham The Duy <phamtheduy153@gmail.com>
2017-08-31 11:40:33 +0700

    working HoC re-render with map state to locale props workaround

--- screens/ExampleHoC.js
+++ screens/ExampleHoC.js
@@ -1,22 +1,15 @@
 import { connect } from 'react-redux'
 import ExampleComponent from './ExampleComponent'
-
-/*
-import { compose as recompose } from 'recompose'
-
-export default recompose(
-  connect(
-    (state, ownProps) => ({
-    }),
-    (dispatch, ownProps) => ({
-    })
-  )
-)(ExampleComponent)
-*/
+import LocalizationRedux from '../redux/App/LocalizationRedux'
 
 export default connect(
-  (state, ownProps) => ({
-  }),
+  (state, ownProps) => {
+    const inState = LocalizationRedux.getReducerState(state)
+    const locale = LocalizationRedux.getCurrentLocale(inState)
+    return {
+      locale
+    }
+  },
   (dispatch, ownProps) => ({
   })
 )(ExampleComponent)
