e43671d43cc630b891cf3998cf9ca0a9e2b60e6d
Pham The Duy <phamtheduy153@gmail.com>
2017-08-31 11:41:23 +0700

    Add demo navigation code - react-native-globalize not working inside navigation

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Provider } from 'react-redux'
+import { StackNavigator } from 'react-navigation'
 import LocalizationWrapper from './localization'
 import store from './redux'
 import ExampleComponent from './screens/ExampleComponent'
 import ExampleHoC from './screens/ExampleHoC'
+
+const Screen = () => (
+  <View style={{flex: 1}}>
+    <ExampleComponent style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} />
+    <ExampleHoC style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} />
+  </View>
+)
+
+const StackNav = StackNavigator({
+  Index: {
+    screen: Screen
+  }
+}, {
+  initialRouteName: 'Index',
+  headerMode: 'none'
+})
 
 export default class App extends React.Component {
   render () {
@@ -12,8 +29,8 @@ export default class App extends React.Component {
       <Provider store={store}>
         <LocalizationWrapper>
           <View style={{flex: 1}}>
-            <ExampleComponent style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} />
-            <ExampleHoC style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} />
+            <Screen />
+            <StackNav style={{flex: 1}} />
           </View>
         </LocalizationWrapper>
       </Provider>
